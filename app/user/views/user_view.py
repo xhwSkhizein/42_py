bd16f6225cabb5a27bdaa82fabc6945876c00295
hongwei <skhizein0706@gmail.com>
2015-11-08 18:52:46 +0800

    add entity id encrypt

--- app/user/views/user_view.py
+++ app/user/views/user_view.py
@@ -1,12 +1,16 @@
 #! -*- coding:utf8 -*-
 # user view
 #
-import base64
+import sys
+sys.path.append("../..")
+from utils.entity_id_helper import EntityIdHelper
+
 
 class UserView(object):
 
     def __init__(self, user_obj):
-        self.id = base64.b64encode(str(user_obj._id) + str(user_obj._create_time) + "salt")
+        # FIXME 抽出encode和decode部分
+        self.id = EntityIdHelper().encode(user_obj._id, "User")
         self.username = user_obj.username_raw() if user_obj.username_raw() else user_obj._username
         self.createTime = str(user_obj._create_time)
         self.description = user_obj.description()
